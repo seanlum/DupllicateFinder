adfc37bdfa9ab65f6a453cdb8e3d38a17bdc07c0
seanlum <seanjklum@gmail.com>
2024-09-27 05:52:08 -0700

    [master v0.0.6]
    	- Got the scanner working on big directories
    	- Got the scanner to use multiprocessing
    		- Using a pool.starmap
    		- May go back to multiprocess.Process for increased workload
    	- Got the database working
    		- cannot use DB Browser while the database is being written to
    TODO:
    	- Multicore test, 100 processes, 10 tasks
    	- 4-6 Hour scan or 9TB Scan completion whichever happens first

--- DupllicateFinder/scan/DFScanner.py
+++ DupllicateFinder/scan/DFScanner.py
@@ -4,6 +4,11 @@ import os
 import hashlib
 import json
 import traceback
+import multiprocessing
+import psutil
+import time
+import sqlite3
+
 sys.path.append(os.path.abspath('../../DupllicateFinder'))
 
 from DupllicateFinder.config import DFConfig
@@ -20,6 +25,18 @@ class DFScanner(metaclass=DFSingletonMeta):
             self.hash_algorithm = hash_algorithm
         else:
             self.hash_algorithm = 'sha1'
+        self.num_cores = multiprocessing.cpu_count()
+        self.processes = []
+        # self.manager = multiprocessing.Manager()
+        #self.temp_path_store = self.manager.list()
+        #self.temp_store = self.manager.list()
+        # self.lock = multiprocessing.Lock()
+        self.output_results = []
+        self.temp_lim = 1000
+        self.process_limit = 100
+        self.task_limit = 10
+        self.max_open_files = 1000
+        self.i = 0
 
     def item_in_list(self, item_to_check, list_to_check):
         try:
@@ -36,8 +53,7 @@ class DFScanner(metaclass=DFSingletonMeta):
     # 3 means Verbose: Entry Names and Hashes
     # 4 means Debug: Verbose + TimeDate Data        
     def log_statement(self, log_verbosity=0, log_string='', log_end=None):
-        print(log_verbosity, log_string)
-        self.logger.log_line(log_verbosity, log_string)
+        self.logger.log_line(log_verbosity, '[' + str(os.getpid()) + '] ' + log_string)
 
     def digest_get_instance(self, algorithm=None):
         if algorithm == None:
@@ -57,102 +73,148 @@ class DFScanner(metaclass=DFSingletonMeta):
         else:
             os.error('Invalid algorithm')
             exit()
-            
-    def digest_file(self, input_file_name=os.DirEntry, algorithm=None):
-        if algorithm == None:
-            algorithm = self.hash_algorithm
-        if input_file_name == '':
-            os.error('Invalid file name')
-            exit()
-        else:
-            try:
-                with open(input_file_name, 'br') as file_handle:
-                    dot_tick = 0
-                    dot_tick_mod = (1024 * 2048) * 1.5
-                    dot_tick_print_mod = 1024 * 32
-                    hash_digest_algorithm = self.digest_get_instance(algorithm)
-                    # self.log_statement(3, 'Hashing:\n.', log_end='')
-                    for file_content_line in file_handle:
-                        hash_digest_algorithm.update(file_content_line)
-                        dot_tick = dot_tick + 1
-                        if (dot_tick % dot_tick_mod) == 0:
-                            pass
-                            # self.log_statement(3, '', log_end='\n')
-                        if (dot_tick % dot_tick_print_mod) == 0:
-                            pass
-                            # log_statement(3, '.', log_end='')
-                    file_handle.close()
-                    hash_ret_data = hash_digest_algorithm.hexdigest()
-                    # log_statement(3, '')
-                    # self.log_statement(3, str('Result Hash:\t' + hash_ret_data))
-                    return hash_ret_data
-            except OSError:
-                self.log_statement(4, 'Skipping: ' + input_file_name)
-            except ValueError:
-                self.log_statement(4, 'Skipping (Could not digest): ' + input_file_name)
-                
-    def scan_directory(self, input_path_name=''):
-        if input_path_name == '':
-            os.error('Invalid path name')
-            exit()
-        else:
-            try:
-                self.log_statement(3, 'Scanning: ' + str(input_path_name))
-                starting_point = os.scandir(input_path_name)
-                results = {
-                    "path": input_path_name,
-                    "files": [],
-                    "directories": [],
-                }
-                for dir_entry in starting_point:
-                    if dir_entry.is_file():
-                        results['files'].append(dir_entry)
-                    elif dir_entry.is_dir():
-                        results['directories'].append(dir_entry)
-                    else:
-                        os.error('Neither file nor directory')
-                starting_point.close()
-                return results
-            except OSError:
-                self.log_statement(3, 'Skipping ' + str(input_path_name))
-                # os.error('Could not open directory or file for reading')
-            except Exception as e:
-                self.log_statement(3, 'Skipping ' + str(input_path_name))
-                # os.error('Could not scan directory')
-                #os.error(traceback.format_exc())
-                # exit()
-             
 
-    def add_hash_storage_entry(self, hash_storage, hash_value, dir_entry_object):
-        not_present = 0
-        if (hash_value == None) or (hash_value == ''):
-            return
+    def close_open_file_descriptors(self):
+        process = psutil.Process()
+        open_files = process.open_files()
+    
+        for open_file in open_files:
+            fd = open_file.fd
+            try:
+                os.close(fd)
+                self.log_statement(4, f"Closed file descriptor: {fd}")
+            except OSError as e:
+                self.log_statement(4, f"Error closing file descriptor {fd}: {e}")
+        #process.terminate()
+
+    def close_all_file_descriptors(self):
+        for process in self.processes:
+            try:
+                process = psutil.Process(process.pid)
+                try:
+                    for open_file in process.open_files():
+                        print(str(process.pid) + ' - ' + open_file.path)
+                except:
+                    pass
+            except psutil.NoSuchProcess:
+                process.terminate()
+                self.processes.remove(process)
+            
+    def digest_file(self, input_file_name, algorithm=None):
+        self.log_statement(4, 'Processing: ' + input_file_name)
+        algorithm = algorithm or self.hash_algorithm
+        hash_ret_data = None
         try:
-            if hash_storage[hash_value] and (hash_storage[hash_value].append is not None):
-                pass
+            with open(input_file_name, 'br') as file_handle: 
+                self.log_statement(4, 'Opened ' + str(self.get_open_file_descriptors()))
+                """ may need this for a ticker later
+                dot_tick = 0
+                dot_tick_mod = (1024 * 2048) * 1.5
+                dot_tick_print_mod = 1024 * 32
+                """
+                hash_digest_algorithm = self.digest_get_instance(algorithm)
+                for file_content_line in file_handle:
+                    hash_digest_algorithm.update(file_content_line)
+                hash_ret_data = hash_digest_algorithm.hexdigest()
+                self.log_statement(3, hash_ret_data + ' - ' + input_file_name)
+                self.log_statement(4, 'Closed ' + str(self.get_open_file_descriptors()))
+        except OSError:
+            self.log_statement(4, 'Skipping: ' + input_file_name)
+        except ValueError:
+            self.log_statement(4, 'Skipping (Could not digest): ' + input_file_name)
+        self.close_open_file_descriptors()
+        return ( input_file_name, hash_ret_data )
+
+    def db_worker(self, temp_store):
+        self.log_statement(4, 'DB Worker lock')
+        #self.lock.acquire()
+        with sqlite3.connect('hashes.db') as conn:
+            conn.execute('PRAGMA journal_mode=WAL;')
+            conn.execute('CREATE TABLE IF NOT EXISTS paths (path TEXT PRIMARY KEY, hash_key TEXT);')
+            count = conn.execute('SELECT COUNT(*) FROM paths').fetchone()[0]
+            self.log_statement(2, 'DB Worker: ' + str(count) + ' entries')
+            #print('Writing 100 DB Writes')
+            cursor = conn.cursor()
+            query = 'INSERT OR IGNORE INTO paths (path, hash_key) VALUES (?, ?)'
+            cursor.executemany(query, temp_store)
+            self.log_statement(3, 'DB Worker: ' + str(len(temp_store)) + ' entries')
+            #print('Wrote 100 DB Writes')
+            conn.commit()
+            self.output_results[:] = []
+        # self.log_statement(4, 'DB Worker unlocking')
+        # self.close_open_file_descriptors()
+        # self.lock.release()
+        # self.log_statement(4, 'DB Worker unlock')
+
+    def add_hash_storage_entry(self, hash_value, dir_entry_object):
+        self.log_statement(4, 'self.add_hash_storage_entry')
+        if hash_value and dir_entry_object:
+            self.log_statement(4, 'add hash storage lock')
+            self.log_statement(3, '[' + str(len(self.temp_store)) +  '] ' + hash_value + ' - ' + dir_entry_object)
+            self.temp_store.append((dir_entry_object, hash_value))
+            if len(self.temp_store) >= 250:
+                #print('Queued 100 DB writes')
+                self.db_worker(self.temp_store)
+                self.temp_store[:] = []
+    
+    def worker_function(self, entries):
+        self.log_statement(4, 'self.worker_function')
+        # temp_path = self.entry[0] + os.path.sep + filename
+        # self.temp_path_store.append(temp_path)
+        self.log_statement(4, 'self.worker_function Entering pool')
+        with multiprocessing.Pool(processes=self.process_limit, maxtasksperchild=self.task_limit) as pool:
+            self.log_statement(4, 'self.worker_function pool started')
+            # self.log_statement(4, 'self.temp_path_store limit reached')
+            self.log_statement(4, 'calling pool.starmap')
+            #self.start_process_with_retry(self.digest_file, args=(filepath,))
+            results = pool.starmap(self.digest_file, [ (digestfile, ) for digestfile in entries ] )
+            #for process in self.processes:
+            #    process.join()
+            #self.temp_path_store[:] = []
+        if len(results) > 0:
+            self.output_results.extend(results)
+        if len(self.output_results) > 250:
+            self.db_worker(results)
+
+
+    # Function to get the number of open file descriptors
+    def get_open_file_descriptors(self):
+        self.log_statement(4, 'self.get_open_file_descriptors')
+        process = psutil.Process()
+        try:
+            # On Unix-like systems, use num_fds()
+            num_fds = process.num_fds()
+        except AttributeError:
+            # On Windows, use open_files() and count the number of open files
+            num_fds = len(process.open_files())
+        return num_fds
+   
+    # Function to start a process with retry mechanism
+    def start_process_with_retry(self, target_func, args=()):
+        self.log_statement(4, 'self.start_process_with_retry')
+        while True:
+            num_fds = self.get_open_file_descriptors()
+            #print(num_fds)
+            if num_fds < self.max_open_files:
+                p = multiprocessing.Process(target=target_func, args=args)
+                self.processes.append(p)
+                p.start()
+                return p
             else:
-                not_present = 1
-        except KeyError:
-            not_present = 1
-        if not_present != 0:
-            hash_storage[hash_value] = list()
-        hash_storage[hash_value].append(dir_entry_object)
-        if (len(hash_storage[hash_value]) > 1):
-            self.log_statement(3, 'Duplicate: ' + hash_value)
-            for entry in hash_storage[hash_value]:
-                self.log_statement(3, '\t' + entry)
-            self.log_statement(3, '')
-        
+                self.log_statement(4, f"Resource limit reached: {num_fds} open file descriptors. Retrying...")
+                self.close_all_file_descriptors()
+                self.log_statement(4, f"Closed all file descriptors. Retrying...")
+                # time.sleep(1)
+
     def enumerate_directory(self, input_path_name=''):
-        hash_storage = {}
+        self.log_statement(4, 'self.enumerate_directory')
         for entry in os.walk(input_path_name):
             if (os.path.isdir(entry[0])):
-                for entryfile in entry[2]:
-                    temp_path = entry[0] + os.path.sep + entryfile    
-                    # self.log_statement(4, 'Processing: ' + temp_path)    
-                    output_hash = self.digest_file(temp_path)
-                    self.add_hash_storage_entry(hash_storage, output_hash, temp_path)
-        return hash_storage
+                self.entry = entry
+                self.log_statement(4, 'calling self.worker_function')
+                self.worker_function([ entry[0] + os.path.sep + sub for sub in entry[2] ])
+
+        return self.hash_storage
 
     def find_duplicates_json(self, directory_to_search):
         dup_data = self.enumerate_directory(directory_to_search)
--- run-cli.py
+++ run-cli.py
@@ -4,7 +4,9 @@ from DupllicateFinder.config import DFConfig
 from DupllicateFinder.util import DFLogger
 from DupllicateFinder.scan import DFScanner
 
-logger = DFLogger(DFConfig.DF_APPLICATION_LOG_LEVEL, output='stdout')
+logger = DFLogger(2, output='stdout')
 scanner = DFScanner(logger, 'sha1')
 duplicates_json = scanner.find_duplicates_json('/mnt')
+with open('output.json', 'w') as f:
+    f.write(duplicates_json)
 print(duplicates_json)
